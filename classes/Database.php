5eb37bffba76aa94e3c4678e3e553cd82fe5849c
Israel Agbo <israelagbo83@gmail.com>
2025-12-01 23:45:39 +0000

    Modifying the database configuration file with strict type setting

--- classes/Database.php
+++ classes/Database.php
@@ -1,32 +1,41 @@
 <?php
+    declare(strict_types=1);
 
-    class Database {
-        private $host = "localhost"; 
-        private $user = "root";
-        private $password = "";
-        private $database = "mini_crud_mvc";
-        private $charset = "utf8mb4";
+use PDO;
 
-        private $conn;
+use PDOException;
 
-        public function connect() {
-            if($this->conn === null) {
-                $dsn = "mysql:host={$this->host};dbname={$this->database};charset={$this->charset}";
 
-                // Try - Catch Block
-                try {
-                    $this->conn = new PDO($dsn, $this->user, $this->password);
-                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    
-                } catch (PDOException $e) {
-                    echo "Database connection failed: " . $e->getMessage();
+class Database {
+    
+    private string $host = "localhost"; 
+    private string $user = "root";
+    private string $password = "";
+    private string $database = "mini_crud_mvc";
+    private string $charset = "utf8mb4";
 
-                }
+    private ?PDO $conn = null;
 
-                return $this->conn;
+    public function connect(): PDO
+    {
+        if($this->conn === null) {
+            $dsn = "mysql:host={$this->host};dbname={$this->database};charset={$this->charset}";
+
+            // Try - Catch Block
+            try {
+                $this->conn = new PDO($dsn, $this->user, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+            } catch (PDOException $e) {
+                echo "Database connection failed: " . $e->getMessage();
+
             }
-        }
 
+           
+        }
+        return $this->conn;
     }
+
+}
 
 ?>

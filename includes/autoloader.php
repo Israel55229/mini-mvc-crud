ab436beee0a2779276996cbc89dc648b3652a4fb
Israel Agbo <israelagbo83@gmail.com>
2025-12-04 17:15:45 +0000

    Modified the autoloader to add path to the controller directory

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -1,15 +1,24 @@
-<?php 
-    spl_autoload_register("myAutoloader");
+<?php
+spl_autoload_register("myAutoloader");
 
-    function myAutoloader($newClass) {
-        $filePath = __DIR__ . "/../classes/" . $newClass . ".php";
-        
-        if(file_exists($filePath)) {
-            include_once $filePath;
-        }
-        else {
-            throw new Exception("Class file is not found: " .  $filePath);
+function myAutoloader($className)
+{
+    // Possible folders to search
+    $paths = [
+        __DIR__ . "/../classes/",
+        __DIR__ . "/../controllers/",
+    ];
+
+    foreach ($paths as $path) {
+        $file = $path . $className . ".php";
+
+        if (file_exists($file)) {
+            include_once $file;
+            return;
         }
     }
 
+    // If not found in any folder
+    throw new Exception("Class file not found: " . $className);
+}
 ?>
